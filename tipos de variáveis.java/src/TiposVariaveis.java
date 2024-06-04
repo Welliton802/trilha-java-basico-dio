50424236e6e9ed5cf1d6ae017eae71063080dc46
Welliton de Lima <lwelliton883@gmail.com>
2024-06-04 20:31:25 -0300

    novas alterações foram feita

--- tipos de variáveis.java/src/TiposVariaveis.java
+++ tipos de variáveis.java/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //estudar 
+        //tipos primitivos
+        //estudar a classa string que representa texto na aplicação 
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short bumeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        // é normal que a variael possa mudar ao longo do codigo
+        numero =2;
+
     }
 }
