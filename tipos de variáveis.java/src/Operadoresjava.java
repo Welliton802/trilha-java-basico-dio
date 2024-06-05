61552341e6b0498390143c31c12dd70c545f5270
Welliton de Lima <lwelliton883@gmail.com>
2024-06-05 11:47:30 -0300

    algumas alterações é novos aprendizados

---
+++ tipos de variáveis.java/src/Operadoresjava.java
@@ -0,0 +1,58 @@
+public class Operadoresjava {
+    public static void main(String[] args) {
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+        // concatenação
+
+        // qual o resultado das expressoes abaixo?
+        //String concatenacao = "?";
+
+        //concatenacao = 1 + 1 + 1 + "1";
+        //System.out.println(concatenacao);
+        //concatenacao = 1 + "1" + 1 + 1;
+        //System.out.println(concatenacao);
+        //concatenacao = 1 + "1" + 1 + "1";
+        //System.out.println(concatenacao);
+        //concatenacao = "1" + 1 + 1 + 1;
+        //System.out.println(concatenacao);
+        //concatenacao = "1" + (1 + 1 + 1);
+        //System.out.println(concatenacao);
+        //outro exenplo
+        //int numero = 5;
+
+         //numero = - numero;
+
+        //System.out.println(numero);
+
+        //modo de deixar o numero positivo
+        //numero = numero* -1;
+
+        //System.out.println(numero);
+
+        //maneira de usar o operador ternario
+        //int a,b;
+        //a=6;
+        //b=6;
+
+        //String resultado = a==b ?"verdadeiro" : "false";
+
+        //System.out.println(resultado);
+
+        //int numero1 =1;
+        //int numero2 =2;
+
+        //boolean simNao = numero1 == numero2;
+
+        //System.out.println("numero é igual a numeroDois" + simNao);
+
+        //simNao = numero1 != numero2;
+
+        //System.out.println("numeroUm é diferete a numeroDois? " + simNao);
+
+        String nomeUm = "WELLITON";
+        String nomeDois = "WELLITON";
+        //esse método compara objetos se são iguais
+        System.out.println(nomeUm.equals(nomeDois));
+
+    }
+
+}
